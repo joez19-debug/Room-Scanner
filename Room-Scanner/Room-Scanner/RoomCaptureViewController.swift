e591c61e30f9b795ac3fcb7e2a4970a519bb19a5
joez19-debug <edwardziegler19@gmail.com>
2025-11-28 20:29:15 -0500

    Fix RoomScanViewModel imports

---
+++ Room-Scanner/Room-Scanner/RoomCaptureViewController.swift
@@ -0,0 +1,43 @@
+import UIKit
+import RoomPlan
+
+final class RoomCaptureViewController: UIViewController, RoomCaptureSessionDelegate {
+    private let roomCaptureView = RoomCaptureView(frame: .zero)
+    private let captureSession = RoomCaptureSession()
+    weak var viewModel: RoomScanViewModel?
+
+    override func loadView() {
+        view = roomCaptureView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        captureSession.delegate = self
+        roomCaptureView.captureSession = captureSession
+    }
+
+    func startSession() {
+        let configuration = RoomCaptureSession.Configuration()
+        captureSession.run(configuration: configuration)
+        viewModel?.isScanning = true
+    }
+
+    func stopSession() {
+        captureSession.stop()
+        viewModel?.isScanning = false
+    }
+
+    func finishSession() {
+        captureSession.stop()
+    }
+
+    func captureSession(_ session: RoomCaptureSession, didUpdate sessionData: CapturedRoom) {
+        // Keep track of the latest captured room in case the delegate wants live updates.
+        viewModel?.capturedRoom = sessionData
+    }
+
+    func captureSession(_ session: RoomCaptureSession, didEndWith data: CapturedRoom, error: (any Error)?) {
+        viewModel?.capturedRoom = data
+        viewModel?.isScanning = false
+    }
+}
