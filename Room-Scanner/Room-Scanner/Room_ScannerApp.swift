afc4391ed00b97a6cb418e7283a4065b98a52e67
joez19-debug <edwardziegler19@gmail.com>
2025-11-28 19:22:36 -0500

    Fresh Xcode project after fixing missing project.pbxproj

---
+++ Room-Scanner/Room-Scanner/Room_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Room_ScannerApp.swift
+//  Room-Scanner
+//
+//  Created by Joe Ziegler on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Room_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
